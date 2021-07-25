47c305a8c018dfea05143d5c03687045d32464cd
sxynix25 <85663597+sxynix25@users.noreply.github.com>
2021-07-25 18:17:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,7 @@ const config = require('./config.json')
   client.on('message', async message => {
 	if (message.content === '!befehle') {
 		await message.reply('die befehle gibt es alles');
-	} else if (message.content === '!join') {
-		await message.reply('nein');
-	} else if (message.content === '!left') {
-		await message.reply('kann ich nicht');
-	} else if (message.content === '!start') {
-		await message.reply('ich kann es nicht');
-	} else if (message.content === '!stop') {
-		await message.reply('gerne');
-	} else if (message.content === '!next') {
-		await message.reply('ich möchte das hören');
-	}
-});
-async function play(voiceChannel) {
-	const connection = await voiceChannel.join(860134301551296532);
-	connection.play('audio.mp3');
+
 }
 
 
